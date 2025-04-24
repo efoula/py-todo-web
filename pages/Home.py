c0485d69fa9633105496221f3a948a6a3b30e3c1
Eslam Foula <efoulae@gmail.com>
2025-04-24 15:55:25 +0200

    New files structure / add pages / change paths for import

--- pages/Home.py
+++ pages/Home.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
-import functions as f
+from app import functions as f
 
 # st.title("")
 st.subheader("ToDo Application")
